762afa0e5b5ef29d857c5103698138029328e929
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-10-04 11:20:23 -0300

    adicionar os arquivos do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,29 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import QrCode from './assets/images/image-qr-code.png'
+import "./styles/page.css";
+
+export const metadata = {
+  title: "Component qr code",
+  description: "qr code component",
+  author: 'João Vitor de Souza',
+  icons: {
+    icon:['/favicon/favicon.png']
+  }
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="w-100 h-100 d-flex align-items-center justify-content-center">
+      <div className="card p-3 pb-5 rounded-3 border-0 shadow text-center position-relative">
+        <Image src={QrCode} className="w-100 h-100 mb-3 rounded-3" alt="image of qr code" />
+        <h1 className="fs-5 fw-bold mb-3 title">Improve your front-end skills by building projects</h1>
+        <p className="paragrafy m-auto">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <div className="position-absolute top-0 right-0 bg-white shadow p-4 largura rounded-2 d-flex gap-4 align-items-center justify-content-center">
+          <p className="fw-bold m-0">Component qr code</p>
+          <i className="bi bi-qr-code-scan"></i>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
