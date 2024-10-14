70fac6dc5fb6e023d622f3dd5965dea4b2282985
João Vitor de Souza <143558833+JoaoVitor2004@users.noreply.github.com>
2024-10-14 19:56:33 -0300

    trocar a cor de fundo do elemento escondido (#2)

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
         <Image src={QrCode} className="w-100 h-100 mb-3 rounded-3" alt="image of qr code" />
         <h1 className="fs-5 fw-bold mb-3 title">Improve your front-end skills by building projects</h1>
         <p className="paragrafy m-auto">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-        <div className="position-absolute top-0 right-0 bg-white shadow p-4 largura rounded-2 d-flex gap-4 align-items-center justify-content-center">
+        <div className="position-absolute top-0 right-0 bg-dark text-white shadow p-4 largura rounded-2 d-flex gap-4 align-items-center justify-content-center">
           <p className="fw-bold m-0">Component qr code</p>
           <i className="bi bi-qr-code-scan"></i>
         </div>
